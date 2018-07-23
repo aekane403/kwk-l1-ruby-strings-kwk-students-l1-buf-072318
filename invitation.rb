ff91a72f65dcd975cf7875cab024a2168d2d5f9e
Abigail Kane <aekane403@gmail.com>
2018-07-23 20:35:03 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -26,8 +26,8 @@ host_name = gets.chomp
 
 puts "Dear #{guest_name},
 
-You are cordially invited to the {Best Halloween Party Ever on October 31 at 6pm. Please RSVP no later than October 30.
+You are cordially invited to the #{party_name} on #{date} at #{time}. Please RSVP no later than October 30.
 
 Sincerely,
 
-Harry Potter"
+#{host_name}"
